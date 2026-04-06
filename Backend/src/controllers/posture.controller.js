f98228d9a923fd99555c0cb823fc3a0487491c25
jaeryang <anjaeryang@gmail.com>
2026-04-06 12:39:28 +0900

    Fix: DB연결 오류 수정 및 API 연결코드 수정

--- Backend/src/controllers/posture.controller.js
+++ Backend/src/controllers/posture.controller.js
@@ -1,5 +1,6 @@
 const postureService = require('../services/posture.service')
 
+// 자세 데이터 저장
 const savePostureData = async (req, res, next) => {
   try {
     const result = await postureService.save(req.body)
@@ -7,6 +8,7 @@ const savePostureData = async (req, res, next) => {
   } catch (e) { next(e) }
 }
 
+// 세션별 자세 기록 조회
 const getPostureBySession = async (req, res, next) => {
   try {
     const result = await postureService.getBySession(req.params.session_id)
